31ca8d713b319733408a57fe0eb35c6f0340679f
Juan Martín Quintero Ballí <103448789+Jumaquba@users.noreply.github.com>
2022-06-08 15:41:21 -0500

    create model Employee
    Co-authored-by: @acorzo <alexecorzo@gmail.com>
    Co-authored-by: @MatiasDevJam <matias.devjam@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_07_221229) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_08_202030) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_07_221229) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "position"
+    t.string "employee_number"
+    t.string "private_number"
+    t.boolean "active"
+    t.bigint "branch_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["branch_id"], name: "index_employees_on_branch_id"
+  end
+
+  add_foreign_key "employees", "branches"
 end

11f2eb098b9ff4a0a7560d696646190f49b926b4
acorzo <alexecorzo@gmail.com>
2022-06-17 17:53:56 -0500

    Controller and views for admin
    Co-authored-by: @jumaquba <jumaquba@gmail.com>

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,21 @@
+class RegistrationsController < ApplicationController
+  # instantiates new user
+  def new
+    @admin = Admin.new
+  end
+  def create
+    @admin = Admin.new(admin_params)
+    if @admin.save
+    # stores saved user id in a session
+      session[:admin_id] = @admin.id
+      redirect_to root_path, notice: 'Successfully created account'
+    else
+      render :new
+    end
+  end
+  private
+  def admin_params
+    # strong parameters
+    params.require(:admin).permit(:name, :password, :password_confirmation)
+  end
+end
